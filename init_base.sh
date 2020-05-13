c2cd81b6f0c7704db6f6af502a7a9e8cb9488596
Attila Badi <attila.badi@gmail.com>
2020-05-13 09:02:28 +0000

    Reusing account from previous django project

--- init_base.sh
+++ init_base.sh
@@ -1,5 +1,5 @@
-git config --global user.name "Attila Badi"
-git config --global user.email "attila.badi@gmail.com"
-git config credential.helper store
-git remote add origin https://github.com/badiattila/ecommerce-django-badiattila.git
-git commit -u origin master
+# git config --global user.name "Attila Badi"
+# git config --global user.email "attila.badi@gmail.com"
+# git config credential.helper store
+# git remote add origin https://github.com/badiattila/ecommerce-django-badiattila.git
+# git push -u origin master
